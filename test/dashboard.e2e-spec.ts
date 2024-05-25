028f032b539ebdb034cda82a7126e609585a2b3c
matheusc1 <cardoso.matheusbs@gmail.com>
2024-05-24 23:59:38 -0300

    test: create E2E tests

---
+++ test/dashboard.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@playwright/test'
+
+test('display day orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await expect(page.getByText('20', { exact: true })).toBeVisible()
+  await expect(page.getByText('-5% em relação ao dia anterior')).toBeVisible()
+})
+
+test('display month orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await expect(page.getByText('200')).toBeVisible()
+  await expect(page.getByText('+4% em relação ao mês anterior')).toBeVisible()
+})
+
+test('display month canceled orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await expect(page.getByText('5', { exact: true })).toBeVisible()
+  await expect(page.getByText('+7% em relação ao mês anterior')).toBeVisible()
+})
+
+test('display month revenue metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await expect(page.getByText('R$ 432,00')).toBeVisible()
+  await expect(page.getByText('11% em relação ao mês anterior')).toBeVisible()
+})
